d87fa670f67ca7daefbe924f9eedea1390e330e6
Micael Bittencourt <ti.micaelbittencourt@outlook.com>
2019-02-14 12:48:05 -0200

    Iniciando a classe Tabuleiro

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using tabuleiro;
 namespace Xadrez_Console
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Console.WriteLine("");
         }
     }
 }
--- Xadrez_Console/Tabuleiro/Posicao.cs
+++ Xadrez_Console/Tabuleiro/Posicao.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Xadrez_Console.Tabuleiro
+﻿namespace tabuleiro
 {
     class Posicao
     {
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao()
+        {
+        }
+
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha + ", " + coluna;
+        }
     }
 }
