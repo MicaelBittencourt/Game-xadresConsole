b2c2a1b48a144c7aecba44da5957fab349b366dd
Micael Bittencourt <ti.micaelbittencourt@outlook.com>
2019-02-24 17:24:31 -0300

    Testes iniciais e alterações na apresentação da tela

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -10,19 +10,29 @@ namespace Xadrez_Console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-                while (!partida.terminada){
+                while (!partida.terminada)
+                {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
 
+                    Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
                     partida.executarMovomento(origem, destino);
                 }
 
-            }catch(TabuleiraException e)
+            }
+            catch (TabuleiraException e)
             {
                 Console.WriteLine(e.Message);
             }
